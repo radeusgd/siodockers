35d79f38e4e819902b4b15a2db0fef121cc536ed
Radosław Waśko <programistagd@gmail.com>
2018-05-27 16:03:34 +0200

    Create preparedocker.sh

---
+++ preparedocker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Utility script to quickly install docker
+# run as root
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
